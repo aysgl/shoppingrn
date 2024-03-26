bf6053f4d4629483d4fb8d01e8e512ab47512ad2
Aysgl Avcu <aysglavcu@Aysgls-MacBook-Pro.local>
2024-03-27 01:38:52 +0300

    updated

--- src/components/HeartIcon.jsx
+++ src/components/HeartIcon.jsx
@@ -5,7 +5,7 @@ import {COLOR} from '../theme/color';
 
 export default function HeartIcon({favorite, updateFavorites}) {
   useEffect(() => {}, [favorite, updateFavorites]);
-  console.log('heartIcon', favorite, updateFavorites);
+  // console.log('heartIcon', favorite, updateFavorites);
   return (
     <TouchableOpacity onPress={updateFavorites}>
       <Heart
