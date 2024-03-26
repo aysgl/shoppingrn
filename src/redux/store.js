f472fbf2f03de7eb13b5a6f0bd17769359cb1e5f
Aysgl Avcu <aysglavcu@Aysgls-MacBook-Pro.local>
2024-03-27 00:11:15 +0300

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
-import categoriesSlice from './categoriesSlice';
 import productsSlice from './productsSlice';
 import cartsSlice from './cartsSlice';
 import authSlice from './authSlice';
 
 const store = configureStore({
   reducer: {
-    categories: categoriesSlice,
     products: productsSlice,
     carts: cartsSlice,
     auth: authSlice,
